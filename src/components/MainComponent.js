11fb04a93bf90b8eff13dd29c0c2e9831e0158a5
Vu Ngoc Mai <vungocmai322@gmail.com>
2022-06-27 04:07:28 +0700

    Single Page Applications Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -30,6 +30,7 @@ class Main extends Component {
     }
 
     render() {
+
         const HomePage = () => {
             return (
                 <Home
@@ -40,6 +41,13 @@ class Main extends Component {
             );
         }
 
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
+            );
+        };
+
         return (
 
             <div>
@@ -47,6 +55,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
