f4e7ee5d029c7aa495e4051401272b305655e61e
Vu Ngoc Mai <vungocmai322@gmail.com>
2022-06-27 02:58:51 +0700

    Header and Footer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
